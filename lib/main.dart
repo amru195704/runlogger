0ff1367b854e57338fc13443bbe1aaf4af171c1c
amru195704 <maru@y42u.net>
2024-11-13 14:18:42 +0900

    初期ファイル登録

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'map.dart';
+import 'gps.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car), text: "map"),
+                Tab(icon: Icon(Icons.directions_transit), text: "run"),
+                Tab(icon: Icon(Icons.directions_bike), text: "Bike"),
+              ],
+            ),
+            title: const Text('RunLogger'),
+          ),
+          body: const TabBarView(
+            children: [
+              MyMap(),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/map.dart
@@ -0,0 +1,186 @@
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'gps.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:platform_maps_flutter/platform_maps_flutter.dart';
+import 'dart:async';
+
+// info.plistに以下を追加
+// 	<key>MinimumOSVersion</key>
+//	<string>14.5</string>
+//
+
+class MyMap extends StatelessWidget {
+  const MyMap({super.key});
+
+  //const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // 位置情報の初期値
+  double _zoom = 16;
+  double _lat = 33.12;
+  double _lon = 131.789;
+  // 地図の種類
+  MapType _currentMapType = MapType.normal;
+  // マーカーのリスト
+  Map<int,Marker> _markTerable = {};
+  int _markerNo = 0;
+  //
+
+  late PlatformMapController _controller;
+
+  //
+  @override
+  void initState() {
+    super.initState();
+    // 1. Timer.periodic : 新しい繰り返しタイマーを作成します
+    // 1秒ごとに _counterを1ずつ足していく
+    Timer.periodic(
+      // 第一引数：繰り返す間隔の時間を設定
+      const Duration(seconds: 1),
+      // 第二引数：その間隔ごとに動作させたい処理を書く
+      (Timer timer) async {
+        final pos = await getLocation() ;
+        _lat = pos.latitude;
+        _lon = pos.longitude;
+        Marker mak = Marker(
+          markerId: MarkerId('${_markerNo}'),
+          position: LatLng(_lat, _lon), //
+          infoWindow: InfoWindow(title: '${_markerNo}'),
+          icon: BitmapDescriptor.defaultMarker, 
+          );
+        _markTerable[_markerNo] = mak;
+        _markerNo++;
+        if (_markerNo >= 20) {
+          _markerNo = 0;
+        }
+        setState(() {
+            _controller.animateCamera(
+              CameraUpdate.newCameraPosition(
+                CameraPosition(
+                  target: LatLng(_lat, _lon),
+                  zoom: _zoom,
+                ),
+              ),
+            );
+          });
+      },
+    );
+  }
+  //
+
+  void _changeMap() {
+    setState(() {
+      _currentMapType = _currentMapType == MapType.normal
+          ? MapType.satellite
+          : MapType.normal;
+    });
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _zoom += 1.0;
+      _controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(_lat, _lon),
+            zoom: _zoom,
+          ),
+        ),
+      );
+    });
+  }
+  void _decrementCounter() {
+    setState(() {
+      _zoom -= 1.0;
+      _controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(_lat, _lon),
+            zoom: _zoom,
+          ),
+        ),
+      );
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:  
+            PlatformMap(
+              initialCameraPosition: CameraPosition(
+                target: LatLng(_lat, _lon ),
+                zoom: _zoom,
+              ),
+              markers: _markTerable.values.toSet(),
+              //mapType: MapType.satellite,
+              mapType: _currentMapType,
+              // ignore: avoid_print
+              onTap: (location) => print('onTap: $location'),
+              // ignore: avoid_print
+              onMapCreated: (PlatformMapController controller) {
+                _controller = controller;
+              //compassEnabled: true,
+              //onMapCreated: (controller) {
+                Future.delayed(const Duration(seconds: 2)).then(
+                  (_) {
+                    controller.animateCamera(
+                      CameraUpdate.newCameraPosition(
+                        CameraPosition(
+                          target: LatLng(_lat, _lon ),
+                          tilt: 30.0,
+                          zoom: _zoom,
+                        ),
+                      ),
+                    );
+                    controller
+                        .getVisibleRegion()
+                        // ignore: avoid_print
+                        .then((bounds) => print("bounds: ${bounds.toString()}" ));
+                  },
+                );
+              }
+            )
+      ,      
+      floatingActionButton:  Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: _changeMap,
+              tooltip: 'ChangeMap',
+              child: const Icon(Icons.layers),
+            ),
+            FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            ),
+            FloatingActionButton(
+              onPressed: _decrementCounter,
+              tooltip: 'Decrement',
+              child: const Icon(Icons.remove),
+            ),
+          ],
+        ),
+      );
+  }
+}
